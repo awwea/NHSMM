a55f237f834e0aafb9c4ac4dc210f2b65d7cd758
awwea <admin@awwea.com>
2025-10-09 12:06:56 -0500

    removing HMM

--- nhsmm/__init__.py
+++ nhsmm/__init__.py
@@ -1,15 +1,8 @@
-
-from .hmm import MultinomialHMM, GaussianHMM, GaussianMixtureHMM, PoissonHMM
 from .hsmm import MultinomialHSMM, GaussianHSMM, GaussianMixtureHSMM, PoissonHSMM, NeuralHSMM
 from .utilities import utils, constraints, SeedGenerator, ConvergenceHandler
 
 
 __all__ = [
-    'MultinomialHMM', 
-    'GaussianMixtureHMM',
-    'GaussianHMM',
-    'PoissonHMM',
-
     'PoissonHSMM',
     'GaussianHSMM',
     'MultinomialHSMM', 
